58935d9d063cc3452e5edb569e2c1df9ef046f14
Harshith007-k <harshitjammula007@gmail.com>
2024-10-03 17:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,6 @@ st.write(
     """Choose the fruits you want in your custom smoothie!""")
 import streamlit as st
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-
 
 name_on_order =st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
@@ -30,7 +27,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen+'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(ingredients)
