262ecd12c82ebd0a82d4a59798f008b61990da06
Harshith007-k <harshitjammula007@gmail.com>
2024-10-04 11:46:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
-    my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(name_on_order)
+    my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS
     values ('""" +ingredients_string+ """','"""+name_on_order+"""')"""
     time_to_submit = st.button('Submit Order')
     #st.write(my_insert_stmt)
