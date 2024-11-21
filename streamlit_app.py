cc734bfb1a13bf1e38890c80160e07ff62c7ac0d
Harshith007-k <harshitjammula007@gmail.com>
2024-11-21 14:33:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,9 @@ def save_bookings(df):
 
 # Email-sending function
 def send_email(user_email, user_name, room, date, start_time, end_time):
-    sender_email = "19bd1a1021@gmail.com"
-    sender_password = "agvrujrctxxwcggk"
-    smtp_server = "smtp.gmail.com"
+    sender_email = "fahmad@phoenixteam.com"
+    sender_password = "Term@2026"
+    smtp_server = "smtp-mail.outlook.com"
     smtp_port = 587
 
     subject = "Conference Room Booking Confirmation"
