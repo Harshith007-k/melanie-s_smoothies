45ae73e1133b5db452002c25229b098c0315981b
phoenixteam-Analytics <harshitjammula007@gmail.com>
2024-12-27 13:22:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ def save_bookings(df):
 # Email-sending function
 def send_email(user_email, user_name, room, date, start_time, end_time):
     sender_email = "jharshith@phoenixteam.com"
-    sender_password = "qbtmrkwyspwxpbln"
+    sender_password = "tbmdzdqbwznlyzqd"
     smtp_server = "smtp-mail.outlook.com"
     smtp_encryption = "STARTTLS"
     smtp_port = 587
